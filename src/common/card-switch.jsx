48510d16598d30dafad0366a7c2f5f746327fbd2
A-believer <davidabolade29@gmail.com>
2025-05-16 09:43:14 +0100

    feat(contact): add card-switch component with dynamic content
    Introduce a new CardSwitch component to display interactive cards with images and descriptions. The component is integrated into the contact page to showcase reasons to choose K-Lord Technologies. Images and card data are dynamically loaded and animated for better user experience.

---
+++ src/common/card-switch.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const CardSwitch = ({ header, data }) => {
+	const [currentCard, setCurrentCard] = useState(0);
+	const currentImage = data[currentCard].image;
+	return (
+		<section className='contain md:space-y-16 space-y-10'>
+			<h2 className='text-[#0F0F0F] font-inter font-semibold lg:tracking-[-2.4px] tracking-[-1px] lg:text-5xl/[64px] text-[32px]/[44px]'>
+				{header}
+			</h2>
+			<div className='grid lg:grid-cols-2 grid-cols-1 gap-x-20 gap-y-8'>
+				<div className='space-y-9'>
+					{data.map((card, index) => (
+						<div
+							key={index}
+							className='flex items-center gap-x-7 transition-all duration-500'>
+							<div
+								className={`${
+									index === currentCard ? "bg-[#FFD9CC] h-[104px]" : ""
+								}  w-[10px] rounded-[28px]`}
+							/>
+							<button
+								onClick={() => setCurrentCard(index)}
+								className='cursor-pointer text-left text-[#0F0F0F] space-y-3'>
+								<h3
+									className={`font-sora text-2xl/[30px] font-semibold tracking-[-1.04px] ${
+										index !== currentCard && "opacity-40 text-[26px]/8"
+									}`}>
+									{card.title}
+								</h3>
+								<AnimatePresence mode='wait'>
+									{index === currentCard && (
+										<motion.p
+											className='text-base/8 font-medium tracking-[-0.45px] opacity-80'
+											key={card.title}
+											initial={{ opacity: 0, y: -20 }}
+											animate={{ opacity: 1, y: 0 }}
+											exit={{ opacity: 0, y: -20 }}
+											transition={{ duration: 0.5, ease: "easeInOut" }}>
+											{card.desc}
+										</motion.p>
+									)}
+								</AnimatePresence>
+							</button>
+						</div>
+					))}
+				</div>
+
+				<AnimatePresence mode='wait'>
+					<motion.img
+						key={currentImage}
+						src={currentImage}
+						className='rounded-[18px]'
+						alt={data[currentCard]?.image}
+						initial={{ opacity: 0, x: 40 }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0, x: -40 }}
+						transition={{ duration: 0.5, ease: "easeInOut" }}
+					/>
+				</AnimatePresence>
+			</div>
+		</section>
+	);
+};
+export default CardSwitch;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,6 +4,38 @@ import bgSrc from "../assets/imgs/contact-bg.png";
 import ReachOut from "../components/contact/reach-out";
 import Cta from "../components/contact/cta";
 import { Link } from "react-router";
+import CardSwitch from "../common/card-switch";
+import imgI from "../assets/imgs/contact-custom.png";
+import imgII from "../assets/imgs/contact-scalable.png";
+import imgIII from "../assets/imgs/contact-expert.png";
+import imgIV from "../assets/imgs/contact-security.png";
+
+const cardData = [
+	{
+		id: 1,
+		title: "Custom-Built Solutions",
+		desc: "We specialize in developing tailored software for healthcare, public sector, enterprise, and SME clients.",
+		image: imgI,
+	},
+	{
+		id: 2,
+		title: "Scalable Platforms",
+		desc: "Our solutions are built to scale with your organization, ensuring smooth growth.",
+		image: imgII,
+	},
+	{
+		id: 3,
+		title: "Expert Support",
+		desc: "Our team provides ongoing support to help you succeed at every stage of your journey.",
+		image: imgIII,
+	},
+	{
+		id: 4,
+		title: "Security & Compliance",
+		desc: "We ensure all solutions meet the highest standards of data security and regulatory compliance, including HIPAA, SOC 2, and GDPR.",
+		image: imgIV,
+	},
+];
 
 const Contact = () => {
 	return (
@@ -21,9 +53,12 @@ const Contact = () => {
 				bgSrc={bgSrc}
 			/>
 			<ReachOut />
+			<CardSwitch header={"Why Choose K-Lord Technologies?"} data={cardData} />
 			<Cta />
 			<div className='my-10 text-center text-xl/normal font-semibold font-inter contain space-y-2 text-[#667085]'>
-				<h4 className='font-sora text-3xl/10 text-[#0f0f0f]'>Have questions?</h4>
+				<h4 className='font-sora text-3xl/10 text-[#0f0f0f]'>
+					Have questions?
+				</h4>
 				Check out our{" "}
 				<Link to={"/faqs"} className='text-[#F95607] underline font-bold'>
 					FAQ{" "}
