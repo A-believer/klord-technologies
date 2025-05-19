963da89ea6ecd79ed260cf2c90bde375a58e635d
A-believer <davidabolade29@gmail.com>
2025-05-19 23:18:29 +0100

    feat(healthcare-software): add new components and update existing ones for healthcare software solution
    This commit introduces new components such as `CardGrey`, `SlidingCards`, `UseCases`, and `Cta` to enhance the healthcare software solution page. It also updates existing components like `CardSwitch` and `PortfolioView` for better functionality and styling. Additionally, new images and data are added to support these changes.

---
+++ src/common/sliding-cards.jsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+
+const SlidingCards = ({ data }) => {
+	const scrollRef = useRef(null);
+
+	const scroll = (direction) => {
+		if (scrollRef.current) {
+			const scrollAmount = 350;
+			scrollRef.current.scrollBy({
+				left: direction === "left" ? -scrollAmount : scrollAmount,
+				behavior: "smooth",
+			});
+		}
+	};
+	return (
+		<section className='contain md:py-16 py-10 space-y-14'>
+			<h2 className='font-sora text-[#0F0F0F] lg:text-5xl/[50px] text-[32px]/10  font-semibold tracking-[-2.4px] text-center'>
+				Our Development Process
+			</h2>
+			<div
+				ref={scrollRef}
+				className='w-full rounded-[26px] flex md:flex-row flex-col md:items-stretch items-center gap-2.5 overflow-x-auto scrollbar-hide md:scrollbar-default scroll-smooth scrollbar-hide '>
+				{data.map((item) => (
+					<div
+						key={item.id}
+						className='md:w-[457px] w-full shrink-0 bg-[#1C1C1C] rounded-[26px] pl-7 pb-8 font-sora flex flex-col justify-between'>
+						<span className='text-[#F5F7F9]/[0.05] text-[97px]/[135px] font-semibold'>
+							{item.id}
+						</span>
+						<div className=' space-y-3 px-2 mb-auto'>
+							<h3 className='text-[27px]/[37px] font-semibold text-[#F5F7F9] text-wrap'>
+								{item.header}
+							</h3>
+							<p className='text-[#F5F7F9]/[0.8] font-inter max-w-[380px] sm:w-full w-[90%]'>
+								{item.desc}
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+};
+
+export default SlidingCards;
