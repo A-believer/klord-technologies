d9fdb6386eee1c1361d617f5da4b7282b42a9577
A-believer <davidabolade29@gmail.com>
2025-05-21 16:16:18 +0100

    style: adjust text size and alignment in navbar, card, and values components
    The changes include updating the text size in the navbar for better readability, aligning content in the card component for consistency, and improving the layout and spacing in the values section for enhanced visual hierarchy and clarity.

--- src/common/card.jsx
+++ src/common/card.jsx
@@ -2,8 +2,8 @@ const Card = ({content}) => {
 	return (
 		<div
 			
-			className='w-full border-2 border-[#01588E] py-[21px] px-[18px] rounded-[10px] shadow-lg min-h-[250px] flex flex-col items-start justify-between gap-y-5 hover:translate-[-2px] transition-all duration-500'>
-			<img src={content.icon} alt='' />
+			className='w-full border-2 border-[#01588E] py-[21px] px-[18px] rounded-[10px] shadow-lg min-h-[250px] flex flex-col items-center justify-between gap-y-5 hover:translate-[-2px] transition-all duration-500 text-center'>
+			<img src={content.icon} alt='' className="w-fit mx-auto"/>
 			<div className='space-y-2'>
 				<p className='font-sora text-xl/[30px] font-semibold text-[#101828]'>
 					{content.title}
--- src/components/career/values.jsx
+++ src/components/career/values.jsx
@@ -1,43 +1,60 @@
 import img from "../../assets/imgs/career-values.png";
 
-
 const values = [
-  {
-    title: "Integrity & Accountability",
-    description: "We do the right thing, even when no one is watching. Transparency and ethics guide every decision."
-  },
-  {
-    title: "Innovation & Technical Excellence",
-    description: "We push boundaries with intelligent, high-performance products. Every line of code serves a mission."
-  },
-  {
-    title: "Collaboration & Growth",
-    description: "We value team thinking and shared wins. Continuous learning is part of our DNA."
-  },
-  {
-    title: "Client Impact First", 
-    description: "From UX to compliance, every detail is designed to solve real problems and deliver results."
-  }
+	{
+		title: "Integrity & Accountability",
+		description:
+			"We do the right thing, even when no one is watching. Transparency and ethics guide every decision.",
+	},
+	{
+		title: "Innovation & Technical Excellence",
+		description:
+			"We push boundaries with intelligent, high-performance products. Every line of code serves a mission.",
+	},
+	{
+		title: "Collaboration & Growth",
+		description:
+			"We value team thinking and shared wins. Continuous learning is part of our DNA.",
+	},
+	{
+		title: "Client Impact First",
+		description:
+			"From UX to compliance, every detail is designed to solve real problems and deliver results.",
+	},
 ];
 
 const Values = () => {
 	return (
-		<section className='textlg/8 font-medium font-inter text-black/[0.8]'>
-			<div>
-				<h2 className='font-sora text-[#0F0F0F] lg:text-5xl/[50px] text-[32px]/10  font-semibold tracking-[-2.4px]'></h2>
-				<p></p>
+		<section className='contain md:py-16 py-8 md:space-y-14 space-y-8 '>
+			<div className='space-y-4'>
+				<h2 className='font-sora text-[#0F0F0F] lg:text-5xl/[50px] text-[32px]/10 font-semibold tracking-[-2.4px]'>
+					Our Culture & Values
+				</h2>
+				<p className='textlg/8 font-medium font-inter text-black/[0.8] tracking-[-0.18px]'>
+					We are not just a workplace; we build meaningful careers. At K-Lord,
+					you’ll find a culture that respects talent, rewards innovation, and
+					nurtures professional growth.
+				</p>
 			</div>
-			<div>
-				<h2 className='font-sora text-[#0F0F0F] lg:text-5xl/[50px] text-[32px]/10  font-semibold tracking-[-2.4px]'></h2>
+			<div className='md:space-y-14 space-y-8'>
+				<h2 className='font-sora text-[#0F0F0F] lg:text-5xl/[50px] text-[32px]/10  font-semibold tracking-[-2.4px]'>
+					What We Stand For
+				</h2>
 
-				<div className="flex justify-between lg:flex-row flex-col">
-					<ul className="space-y-[34px] max-w-[493px] w-full">
-						<li className="space-y-3">
-							<h5 className='font-sora text-[#0F0F0F] text-2xl/[29.76px]  font-semibold tracking-[-1.04px]'></h5>
-							<p></p>
-						</li>
+				<div className='flex justify-between xl:flex-row flex-col gap-y-9 gap-x-10'>
+					<ul className='space-y-[34px] max-w-[493px] w-full shrink-0'>
+						{values.map((item, idx) => (
+							<li key={idx} className='space-y-3'>
+								<h5 className='font-sora text-[#0F0F0F] text-2xl/[29.76px]  font-semibold tracking-[-1.04px]'>
+									{item.title}
+								</h5>
+								<p className='textlg/8 font-medium font-inter text-black/[0.8] tracking-[-0.18px]'>
+									{item.description}
+								</p>
+							</li>
+						))}
 					</ul>
-					<img src={img} alt='a picture' />
+					<img src={img} alt='a picture' className='object-contain xl:w-full  mx-auto' />
 				</div>
 			</div>
 		</section>
