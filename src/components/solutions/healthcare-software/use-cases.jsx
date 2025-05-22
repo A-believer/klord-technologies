a5503c48f8715a8db634efbbc1f850dde86343b1
A-believer <davidabolade29@gmail.com>
2025-05-22 12:56:16 +0100

    feat(healthcare-software): add use cases section to healthcare software page
    The use cases section now includes a detailed breakdown of the challenge, solution, and result for a healthcare provider using NexaLife Care. This addition provides users with a clearer understanding of the software's impact and benefits.

--- src/components/solutions/healthcare-software/use-cases.jsx
+++ src/components/solutions/healthcare-software/use-cases.jsx
@@ -12,6 +12,37 @@ const UseCases = () => {
 				</p>
 				<NexalifecareImg />
 			</div>
+			<div className='flex items-start justify-around md:gap-12 gap-8'>
+				<div className='space-y-3 max-w-[318px] flex flex-col md:item-start items-center md:text-left text-center'>
+					<h5 className='font-sora text-[27px]/[31px] font-semibold tracking-[-1.04px] w-full'>
+						Challenge
+					</h5>
+					<p className='font-inter text-[15px]/[25.4px] tracking-[-0.45px]'>
+						A healthcare provider faced inefficiencies in patient referral
+						processes.
+					</p>
+				</div>
+
+				<div className='space-y-3 max-w-[318px] flex flex-col md:item-start items-center md:text-left text-center'>
+					<h5 className='font-sora text-[27px]/[31px] font-semibold tracking-[-1.04px] w-full'>
+						Solution
+					</h5>
+					<p className='font-inter text-[15px]/[25.4px] tracking-[-0.45px]'>
+						Implemented NexaLife Care, automating referral workflows and
+						improving scheduling.
+					</p>
+				</div>
+
+				<div className='space-y-3 max-w-[318px] flex flex-col md:item-start items-center md:text-left text-center'>
+					<h5 className='font-sora text-[27px]/[31px] font-semibold tracking-[-1.04px] w-full'>
+						Result
+					</h5>
+					<p className='font-inter text-[15px]/[25.4px] tracking-[-0.45px] '>
+						Achieved a 25% reduction in referral processing time and increased
+						patient satisfaction.
+					</p>
+				</div>
+			</div>
 		</section>
 	);
 };
